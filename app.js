a3f07589f6ebbb1b716d092d15f67dadb5e180c7
Thiago Túlio <ttbrandao_silva@hotmail.com>
2020-11-19 17:51:26 -0300

    connecting API to DB

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// --->>> ROTAS <<<---
 const rotaTickets = require('./routes/tickets');
-
 app.use('/tickets', rotaTickets);
+
+
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
 
 module.exports = app; 
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -8,8 +9,55 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'usando POST dentro da rota de tickets'
+
+    mysql.getConnection(function (error, conn) {
+        if (error)
+            return res.status(500).send({
+                error: error,
+                response: null
+            });
+        
+        conn.query(
+            'INSERT INTO tickets (placa, tarifa) VALUES (?,?)',
+            [req.body.placa, req.body.tarifa],
+            function(error, resultado, field) {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                res.status(201).send({
+                    mensagem: 'Ticket criado com sucesso',
+                    id: resultado.insertId
+                });
+            }
+        )
+    });
+});
+
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto;
+    res.status(200).send({
+        mensagem: 'usando o GET by id',
+        id: id
+    });
+});
+
+router.patch('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto;
+    res.status(200).send({
+        mensagem: 'usando o PATCH by id',
+        id: id
+    });
+});
+
+router.delete('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto;
+    res.status(200).send({
+        mensagem: 'usando o Delete by id',
+        id: id
     });
 });
 
