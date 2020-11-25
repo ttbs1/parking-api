9104774c755218424aaaa1179bc1b1312e6ea088
Thiago Túlio <ttbrandao_silva@hotmail.com>
2020-11-25 01:25:59 -0300

    return tickets via GET

--- routes/tickets.js
+++ routes/tickets.js
@@ -3,14 +3,49 @@ const router = express.Router();
 const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'usando GET dentro da rota de tickets'
+    mysql.getConnection((error, conn) => {
+        if (error)
+            return res.status(500).send({
+                error: error,
+                response: null
+            });
+        
+        conn.query(
+            'SELECT * FROM tickets',
+            (error, result, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                const response = {
+                    length: result.length,
+                    request: {
+                        type: 'GET',
+                        description: 'Return all tickets'
+                    },
+                    tickets: result.map(ticket => {
+                        return {
+                            id: ticket.id,
+                            placa: ticket.placa,
+                            tarifa: ticket.tarifa,
+                            datahora: ticket.datahora,
+                            url: 'http://localhost:3000/tickets/' + ticket.id
+                        }
+                    }) 
+                    
+                }
+                res.status(201).send(response);
+            }
+        )
     });
 });
 
 router.post('/', (req, res, next) => {
 
-    mysql.getConnection(function (error, conn) {
+    mysql.getConnection((error, conn) => {
         if (error)
             return res.status(500).send({
                 error: error,
@@ -20,7 +55,7 @@ router.post('/', (req, res, next) => {
         conn.query(
             'INSERT INTO tickets (placa, tarifa) VALUES (?,?)',
             [req.body.placa, req.body.tarifa],
-            function(error, resultado, field) {
+            (error, result, field) => {
                 conn.release();
                 if (error) {
                     return res.status(500).send({
@@ -28,20 +63,56 @@ router.post('/', (req, res, next) => {
                         response: null
                     });
                 }
+                const response = {
+                    lenght: result.lenght,
+
+                }
                 res.status(201).send({
                     mensagem: 'Ticket criado com sucesso',
-                    id: resultado.insertId
+                    id: result.insertId
                 });
             }
         )
     });
 });
 
-router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto;
-    res.status(200).send({
-        mensagem: 'usando o GET by id',
-        id: id
+router.get('/:id', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error)
+            return res.status(500).send({
+                error: error,
+                response: null
+            });
+        
+        conn.query(
+            'SELECT * FROM tickets WHERE id = ?',
+            [req.params.id],
+            (error, result, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                const response = {
+                    request: {
+                        type: 'GET',
+                        description: 'Return ticket by id'
+                    },
+                    tickets: result.map(ticket => {
+                        return {
+                            id: ticket.id,
+                            placa: ticket.placa,
+                            tarifa: ticket.tarifa,
+                            datahora: ticket.datahora,
+                        }
+                    }) 
+                    
+                }
+                res.status(201).send(response);
+            }
+        )
     });
 });
 
